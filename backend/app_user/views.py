a7655af7d694de3b0a96bebc9a90530fe8fb8e67
mohammadSelimReza <srreza1999@gmail.com>
2024-08-31 06:12:43 +0600

    requirment.txt

--- backend/app_user/views.py
+++ backend/app_user/views.py
@@ -96,7 +96,7 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('login')
+        return redirect('http://localhost:5173/login')
     return redirect('patientRegistration')
       
 
