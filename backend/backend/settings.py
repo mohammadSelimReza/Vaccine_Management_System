a0ebef4c71483e4e39eb082f93c1354cf0f1d1ec
mohammadSelimReza <srreza1999@gmail.com>
2024-08-31 06:21:26 +0600

    postgre

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import environ
+import dj_database_url
 env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,10 +101,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url(
+        default=env('POSTGRESQL'),
+        conn_max_age=600,
+    )
 }
 
 
